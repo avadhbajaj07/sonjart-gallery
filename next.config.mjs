ddb30c84c143e6077329c429a2eb0eab935f283f
Avadh Bajaj <shouryasaad6@gmail.com>
2026-06-12 22:03:59 +0530

    Initial Sonjart gallery commit

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  serverExternalPackages: ['nodemailer'],
+};
+
+export default nextConfig;
